87abd96e984617debd81e62e86b9bcd177872db0
mdsahibx-prog <mdsahibx@gmail.com>
2026-09-20 12:54:24 +0600

    Integrate mobile storefront navigation

--- app/(storefront)/layout.tsx
+++ app/(storefront)/layout.tsx
@@ -2,8 +2,9 @@ import { AssistantButton } from '@/components/assistant/assistant-button'
 import { loadAssistantControlConfig } from '@/lib/assistant/config'
 import { SiteFooter } from '@/components/layout/site-footer'
 import { SiteHeader } from '@/components/layout/site-header'
+import { MobileBottomNav } from '@/components/layout/mobile-bottom-nav'
 
 export default async function StorefrontLayout({ children }: { children: React.ReactNode }) {
   const assistantConfig = await loadAssistantControlConfig()
-  return <><SiteHeader />{children}<AssistantButton enabled={assistantConfig.enabled} maintenanceMode={assistantConfig.maintenanceMode} maintenanceMessage={assistantConfig.maintenanceMessage} assistantName={assistantConfig.agentProfile.agentName || assistantConfig.assistantName} subtitle={assistantConfig.agentProfile.subtitle} buttonLabel={assistantConfig.buttonLabel} maxVisibleProductCards={assistantConfig.maxVisibleProductCards} showQuickPrompts={assistantConfig.showQuickPrompts} welcomeMessage={assistantConfig.agentProfile.welcomeMessage || assistantConfig.welcomeMessage} quickPrompts={assistantConfig.quickPrompts} /><SiteFooter /></>
+  return <><SiteHeader /><div className="pb-16 lg:pb-0">{children}</div><MobileBottomNav /><AssistantButton enabled={assistantConfig.enabled} maintenanceMode={assistantConfig.maintenanceMode} maintenanceMessage={assistantConfig.maintenanceMessage} assistantName={assistantConfig.agentProfile.agentName || assistantConfig.assistantName} subtitle={assistantConfig.agentProfile.subtitle} buttonLabel={assistantConfig.buttonLabel} maxVisibleProductCards={assistantConfig.maxVisibleProductCards} showQuickPrompts={assistantConfig.showQuickPrompts} welcomeMessage={assistantConfig.agentProfile.welcomeMessage || assistantConfig.welcomeMessage} quickPrompts={assistantConfig.quickPrompts} /><SiteFooter /></>
 }
